15f7101f72a91da1412f4806c5fcdcf91c0e2a2a
SonaShaw <sonashawprofessional@gmail.com>
2021-01-22 09:23:13 +0530

    Cyclic rotation

---
+++ Codility/Lesson-2/CyclicRotaion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    class CyclicRotaion
+    {
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 3, 8, 9, 7, 6 };
+            int[] res = Solution(a, 3);
+            foreach(int x in res)
+                Console.Write(x + " ");
+        }
+        private static int[] Solution(int[] a, int k)
+        {
+            int[] res = new int[a.Length];
+            if (a.Length == 0)
+                return a;
+            if (k > a.Length)
+                k = k % a.Length;
+            for(int i=0; i<a.Length; i++)
+            {
+                res[i] = a[(a.Length - k + i) % a.Length];
+            }
+            return res;
+        }
+
+    }
+
+    
+}
