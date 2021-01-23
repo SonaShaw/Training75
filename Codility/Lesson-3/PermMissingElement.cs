15480d856d9630fa84ded3b6261eed6a11a92ce9
SonaShaw <sonashawprofessional@gmail.com>
2021-01-23 21:52:22 +0530

    Perm Missing Element

---
+++ Codility/Lesson-3/PermMissingElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility
+{
+    class PermMissingElement
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 2, 3, 1, 5 };
+            int res = Solution(a);
+            Console.WriteLine(res);
+        }
+
+        private static int Solution(int[] a)
+        {
+            int[] temp = new int[a.Length + 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                int index = a[i]-1;
+                temp[index]++;
+            }
+            int res=Int32.MinValue;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == 0)
+                {
+                    res = i + 1;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
